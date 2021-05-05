2458e422f935fd06dde799d1a00ba00cf2171852
GabrieleBoccarusso <gabrieleboccarusso@gmail.com>
2021-05-05 14:19:22 +0000

    first and porly written working hash table

--- C++/hash_tables.cpp
+++ C++/hash_tables.cpp
@@ -2,16 +2,61 @@
 
 using std::cout;
 
+template <class dtype_val>
+struct hash_value {
+  char key[10];
+  dtype_val val;
+};
+
+template<class val_type, int size>
 class hash_table
 {
+  public:
+    int max_key_size = 10;
+    hash_value<val_type> data[size];
 
-}
+    // key maximum 10 chars
+    int hashing(char *key)
+    {
+      int hash = 0;
+      int i;
+
+      // basic hashing algorithm founded at:
+      // [link]
+      for (i = 0; i < max_key_size; i++)
+      {
+        hash = hash + key[i] * i;
+      }
+
+      hash = hash % size;
+
+      return hash;
+    }
+
+    void set(char *key, int value)
+    {
+      int index = hashing(key);
+      for (int i = 0; i < max_key_size; i++)
+      {
+        data[index].key[i] = key[i];
+      }
+      data[index].val = value;
+      return;
+    }
+
+  
+};
 
 int main()
 {
-  char key[10] = "giovanni";
-  cout << hash(key);
+  hash_table<int, 10> table;
 
+  char k [10] = "gabriele";
+
+
+  table.set(k, 10);
+
+  cout << table.data[5].val;
   return 0;
 }
 
@@ -19,8 +64,6 @@ int main()
 hash function
 int hash (char key [10])
 {
-  int hash = 0;
-  int i;
 
   for (i = 0; i < 10; i++)
   {
