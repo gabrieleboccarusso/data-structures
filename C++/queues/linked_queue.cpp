630349c73dbc97e759b3fee35438984bbb014b95
GabrieleBoccarusso <gabrieleboccarusso@gmail.com>
2021-05-13 07:13:49 +0000

    added first version of the linked queue

---
+++ C++/queues/linked_queue.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template <class dtype>
+class linked_queue
+{
+  private:
+    struct node {
+      dtype data;
+      node * next = NULL;
+    };
+    int length = 0;
+    node * pfirst = NULL; // pointer to the first
+    node * plast = NULL; // pointer to the last
+    
+  public:
+    // constructor
+    linked_queue(dtype value)
+    {
+      node * temp = new node;
+      temp -> data =  value;
+      plast = temp;
+      pfirst = temp;
+      length++;
+    }
+
+    bool isEmpty ()
+    {
+      bool answer = false;;
+        if(plast == NULL && pfirst == NULL)
+          answer = true;
+    
+      return answer;
+    }
+
+    int enqueue(dtype value)
+    {
+      node * temp = new node;
+      temp -> data = value;
+      plast -> next = temp;
+      plast = temp;
+      length++;
+
+      return length;
+    }
+
+    void view_queue()
+    {
+      node * temp = pfirst;
+      int i;
+
+      while (temp != NULL)
+      {
+        cout << i << ": " << temp -> data << endl;
+        temp = temp -> next; 
+      }
+    }
+};
+
+int main()
+{
+  linked_queue<int> my_queue (10);
+  my_queue.enqueue(15);
+  cout << "the queue:\n";
+  my_queue.view_queue();
+  cout << "now enqueued:\n";
+  return 0;
+}
