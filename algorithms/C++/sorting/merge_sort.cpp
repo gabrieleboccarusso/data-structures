1a282ef052a2234a58f8f5afdb28d2697770788a
GabrieleBoccarusso <gabrieleboccarusso@gmail.com>
2021-05-27 15:05:31 +0000

    first basic version of the merge sort

--- algorithms/C++/sorting/merge_sort.cpp
+++ algorithms/C++/sorting/merge_sort.cpp
@@ -3,52 +3,73 @@
 using std::cout;
 using std::endl;
 
-void merge_sort(int arr[], int size);
+int* merge_sort(int *ptr, int size);
+void merge(int *left, int*right, int left_size, int right_size);
 
 int main()
 {
-  int size = 4;
-  int arr[] = {3, 4, 1, 2};
+  int size = 2;
+  int arr[] = {4, 3};
   int *ptr = arr;
 
   merge_sort(arr, size);
+
+  for(int i = 0; i < size; i++)
+  {
+    cout << ptr[i] << " ";
+  }
 
   return 0;
 }
 
 // in those first cases we'll not take in consideration
 // that size may be a non-pair number
-void merge_sort(int *ptr, int size)
+int* merge_sort(int *ptr, int size)
 {
   int i;
-  // if size is 2
-  if (size == 2)
-  {
-    // output the array
-    for(i = 0; i < size; i++)
-      cout << ptr[i] << " ";
-
-    cout << endl;
-  }
-  // else if size is greater than two
-  else if (size > 2)
-  { // create two array with half the size
-    int half_size = size/2;
+  int half_size = size/2;
     int *left = new int[half_size];
     int *right = new int [half_size];
+    int outer_counter = 0;
+  // else if size is greater than 1
+  if (size > 1)
+  { // create two array with half the size
 
     // fill the two arrays
     for (i = 0; i < half_size; i++)
       left[i] = ptr[i];
 
-    int outer_counter = 0;
+
     for (i = half_size; i < size; i++)
     {  
       right[outer_counter] = ptr[i];
       outer_counter++;
     }
-    // call again the function (recurison)
-    merge_sort(left, half_size);
-    merge_sort(right, half_size);
+  }
+
+  merge(left, right, half_size, half_size);
+
+  // groupt the two different arrays together
+  for(i = 0; i < half_size; i++)
+    ptr[i] = left[i];
+
+  outer_counter = 0;
+  for(i = half_size;i < size; i++)
+    ptr[i] = right[outer_counter];
+
+  return ptr; 
+}
+
+void merge(int *left, int*right, int left_size, int right_size)
+{
+  if (left_size == 1 && right_size == 1)
+  {
+    // if the array has just one element
+    if (left[0] > right[0])
+    { // we may have to swap them
+      int temp = right[0];
+      right[0] = left[0];
+      left[0] = temp;
+    }
   }
 }
